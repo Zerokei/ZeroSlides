4a0aa37fd1aad3aa6ad58a534c28dc412cf62044
Chritch <chritch-7@outlook.com>
2024-08-09 17:33:32 +0800

    feat: add single

---
+++ layouts/single.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="slidev-layout single">
+    <div class="my-auto w-full">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<style lang="postcss">
+.slidev-layout.single {
+  /* 居中 */
+  @apply flex items-center justify-center;
+  @apply text-center;
+  h1 {
+    font-family: 'Smiley Sans Oblique';
+    @apply text-6xl mb-5 text-amber-900;
+    @apply inline-block;
+
+    @apply pb-1 pr-5 pl-5;
+  }
+  .footnote-ref {
+    @apply text-3xl;
+  }
+  .footnotes {
+    @apply text-sm;
+  }
+}
+
+
+</style>
